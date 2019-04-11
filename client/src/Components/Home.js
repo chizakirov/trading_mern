555e8d0c1dcdf360b1379cabf653a3f5096fd086
Chi Nguyen <nguyen.chi.byu@gmail.com>
2019-04-10 19:13:41 -0700

    added copyright

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -7,6 +7,9 @@ const Home = () => (
       <h1 id="home-title"> Welcome to the trading simulation app! </h1>
       <h4>Trade your way to the top without the fear of breaking your wallet.</h4>
       <hr></hr>
+      <div className="footer">
+        <p id="copyright">Copyright &#169; Chi Nguyen</p>
+      </div>
     </div>
   </div>
 )
