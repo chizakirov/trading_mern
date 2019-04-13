41676ed80270c3e7142602a6ddf5a1abc872ca4d
Chi Nguyen <nguyen.chi.byu@gmail.com>
2019-04-12 20:19:52 -0700

    pre-heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const routes = require('./server/routes.js'); 
+const path = require('path');
 require("dotenv").config();
 const port = process.env.PORT || 1234;
 const app = express();
@@ -10,7 +11,8 @@ app.use(express.static(__dirname + './server'));
 app.use(express.static( __dirname + '/client/public' ));
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static( __dirname + 'client/build' ));
+  app.use(express.static("client/build"));
+  // app.use(express.static( __dirname + 'client/build' ));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
