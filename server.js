2e6fead3c639b004d7b84cbd8592111eb2629f76
Chi Nguyen <nguyen.chi.byu@gmail.com>
2019-04-12 22:23:35 -0700

    something

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./server/routes.js'); 
 const path = require('path');
-require("dotenv").config();
+const routes = require('./server/routes.js');
+require('dotenv').config();
+
 const port = process.env.PORT || 1234;
 const app = express();
 
 app.use(bodyParser.json());
-// app.use(express.static(__dirname + './server'));
-// app.use(express.static( __dirname + '/client/public' ));
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"));
-  // app.use(express.static( __dirname + 'client/build' ));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-};
+}
 
 routes(app);
 
-app.listen(port, () => console.log("listening on port", port));
+app.listen(port, () => console.log('listening on port', port));
